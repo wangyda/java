b54b2d3ffc4c7a7d2c269a102a1da8138040b4aa
wangyda <unwmo@163.com>
2018-07-01 13:04:52 +0800

    delete pstn

--- dudu-numberroute/src/main/java/dudu/cass/numberroute/repository/PstnRepository.java
+++
@@ -1,9 +0,0 @@
-package dudu.cass.numberroute.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dudu.cass.numberroute.entity.NumberRouteEntity;
-
-public interface PstnRepository extends JpaRepository<NumberRouteEntity,Long> { 
-
-}
